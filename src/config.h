e1b1f06f5934c411c6bbcdb5c24bf35c938127ac
Johan Nilsson <oliver_twistor@hotmail.com>
2019-07-22 17:10:18 +0200

    #2 Moved the microchip CPU frequency to its own file.

---
+++ src/config.h
@@ -0,0 +1,17 @@
+/**
+ * This file contains configuration constants and variables used throughout the
+ * entire application.
+ *
+ * @author Johan Nilsson
+ * @since 0.2.0
+ */
+
+#ifndef CONFIG_H_
+#define CONFIG_H_
+
+/* Set the frequency. */
+#ifndef F_CPU
+#define F_CPU 8000000UL // 8 MHz
+#endif
+
+#endif /* MODES_H_ */
